fb3c31afa622aee077552eea57075382de12dd55
Vempalli_Gowtham_Theja <vempalligowthamtheja@gmail.com>
2023-09-12 22:14:30 +0530

    config: implemented Bun 1.0

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,12 @@ import React from "react";
 
 const Topbar = () => {
   return (
-    <Navbar shouldHideOnScroll className="dark:bg-slate-800 bg-slate-950">
+    <Navbar
+      className="dark:bg-slate-800 bg-slate-950"
+      shouldHideOnScroll
+      maxWidth="2xl"
+      height="5rem"
+    >
       <NavbarBrand>
         <Logo />
         <p className="font-bold text-inherit uppercase text-lg tracking-widest select-none ml-4">
